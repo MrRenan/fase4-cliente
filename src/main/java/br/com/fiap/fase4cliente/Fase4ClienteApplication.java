170fc9e04332b4db794f686f14ca295d34cbedb1
Renan Leite <renan.carli@hotmail.com>
2025-01-19 20:36:48 -0300

    First Commit

---
+++ src/main/java/br/com/fiap/fase4cliente/Fase4ClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase4ClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fase4ClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/fase4cliente/Fase4ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fase4cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fase4ClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
