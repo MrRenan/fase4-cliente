6fa118e39a118bb6d38f97dc8834410adbf3c7a4
Renato Drumond <rm357140@fiap.com.br>
2025-02-07 14:55:40 -0300

    Cadastrando Cliente

--- src/main/java/br/com/fiap/fase4cliente/application/cliente/dtos/ClienteRecordDto.java
+++ src/main/java/br/com/fiap/fase4cliente/application/cliente/dtos/ClienteRecordDto.java
@@ -4,6 +4,17 @@ package br.com.fiap.fase4cliente.application.cliente.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record ClienteRecordDto(@NotBlank String name,
-                               @NotBlank @Email String email)  {
+public record ClienteRecordDto(@NotBlank String name, @NotBlank @Email String email) {
+    public ClienteRecordDto(@NotBlank String name, @NotBlank @Email String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public @NotBlank String name() {
+        return this.name;
+    }
+
+    public @NotBlank @Email String email() {
+        return this.email;
+    }
 }
